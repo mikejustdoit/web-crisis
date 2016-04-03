75d5bf09e1a709c0a8d14d1da33c4b665039cfe0
Mike <mike63443@gmail.com>
2016-04-03 12:26:53 +0100

    Introduce pending scenario for browsing the WWW

---
+++ features/browsing_the_www.feature
@@ -0,0 +1,8 @@
+Feature: browsing the WWW
+
+  @wip
+  Scenario: entering an address
+    Given I have launched the web browser
+    When I enter an address
+    And I click "go"
+    Then the browser should render the web page
---
+++ features/step_definitions/browsing_steps.rb
@@ -0,0 +1,15 @@
+Given(/^I have launched the web browser$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I enter an address$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I click "go"$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^the browser should render the web page$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
