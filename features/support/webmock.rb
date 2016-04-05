d37c9ab6c2d6b5148b83ef8245f97b53c4aef39f
Mike <mike63443@gmail.com>
2016-04-05 18:07:32 +0100

    Stub known requests in Cucumber Before hook
    Lazily opening their respective files

--- features/support/webmock.rb
+++ features/support/webmock.rb
@@ -1,3 +1,15 @@
 require "webmock/cucumber"
 
 WebMock.disable_net_connect!
+
+def raw_responses
+  {
+    "https://lwn.net/" => -> { File.new("features/support/lwn-raw-response.txt") },
+  }
+end
+
+Before do
+  raw_responses.each do |uri, raw_response|
+    stub_request(:get, uri).to_return { raw_response.call }
+  end
+end
