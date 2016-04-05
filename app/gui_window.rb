098d69f1f800ad08d46957b65d10ce9d487bfcb3
Mike <mike63443@gmail.com>
2016-04-05 11:37:28 +0100

    Add address field to GuiWindow
    I think ideally this and the reference to the engine would live in a "model"
    object representing the state of the GuiWindow view, but with only two of them,
    I don't suppose it's a big deal

--- app/gui_window.rb
+++ app/gui_window.rb
@@ -4,10 +4,14 @@ class GuiWindow < Gosu::Window
   def initialize(engine)
     @engine = engine
 
+    @address = ""
+
     super(640, 480)
 
     self.caption = "Web Crisis browser"
   end
+
+  attr_accessor :address
 
   private
 
