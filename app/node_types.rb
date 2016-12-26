42fdd0801cddc51048814af9e3c9d25fbd9a25d3
Mike <mike63443@gmail.com>
2016-12-26 17:11:20 +0000

    Define INLINE_NODES
    In preparation for handling them differently to block-level nodes

--- app/node_types.rb
+++ app/node_types.rb
@@ -1,5 +1,8 @@
 require "block_level_element"
 require "element"
 require "inline_element"
+require "text"
 
 ELEMENTS = [BlockLevelElement, Element, InlineElement]
+
+INLINE_NODES = [InlineElement, Text]
