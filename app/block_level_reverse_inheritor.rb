67df1e61981fedaa64894d11b51cecbf3ec24910
Mike <mike63443@gmail.com>
2017-01-17 08:35:28 +0000

    Introduce BlockLevelReverseInheritor
    So that inline parents inherit their block-level childrens' block-levelness.
    
    I dunno, I think this is how it's supposed to work

---
+++ app/block_level_reverse_inheritor.rb
@@ -0,0 +1,42 @@
+require "node_types"
+
+class BlockLevelReverseInheritor
+  def initialize(**_); end
+
+  def call(node)
+    case node
+    when InlineElement
+      visit_inline_element(node)
+    when *ELEMENTS
+      visit_block_level_element(node)
+    when Text
+      node
+    end
+  end
+
+  private
+
+  def visit_inline_element(node)
+    new_children = node.children.map { |child|
+      call(child)
+    }
+
+    return node unless new_children.any? { |child| child.is_a?(BlockLevelElement) }
+
+    BlockLevelElement.new(
+      node.clone_with(
+        children: new_children,
+      )
+    )
+  end
+
+  def visit_block_level_element(node)
+    new_children = node.children.map { |child|
+      call(child)
+    }
+
+    node.clone_with(
+      children: new_children,
+    )
+  end
+end
---
+++ spec/block_level_reverse_inheritor_spec.rb
@@ -0,0 +1,50 @@
+require "block_level_reverse_inheritor"
+require "block_level_element"
+require "inline_element"
+require "support/shared_examples/visitor"
+
+RSpec.describe BlockLevelReverseInheritor do
+  subject(:visitor) { BlockLevelReverseInheritor.new }
+
+  it_behaves_like "a visitor"
+
+  it_behaves_like "a depth-first tree traverser"
+
+  describe "the returned tree" do
+    let(:root) {
+      InlineElement.new(Element.new(children: [first_child, last_child]))
+    }
+    let(:first_child) {
+      InlineElement.new(
+        Element.new(
+          children: [first_grandchild, middle_grandchild, last_grandchild],
+        )
+      )
+    }
+    let(:first_grandchild) { Text.new(content: "ABC") }
+    let(:middle_grandchild) { BlockLevelElement.new(Element.new) }
+    let(:last_grandchild) { InlineElement.new(Element.new) }
+    let(:last_child) { InlineElement.new(Element.new) }
+
+    let(:returned_root) { visitor.call(root) }
+    let(:returned_first_child) { returned_root.children.first }
+    let(:returned_first_grandchild) { returned_first_child.children.first }
+    let(:returned_last_grandchild) { returned_first_child.children.last }
+    let(:returned_last_child) { returned_root.children.last }
+
+    it "turns inline ancestors of block-level elements block-level" do
+      expect(returned_first_child).to be_a BlockLevelElement
+      expect(returned_root).to be_a BlockLevelElement
+    end
+
+    it "leaves everything else alone" do
+      [
+        returned_first_grandchild,
+        returned_last_grandchild,
+        returned_last_child,
+      ].each do |node|
+        expect(node).not_to be_a BlockLevelElement
+      end
+    end
+  end
+end
