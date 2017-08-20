3360e339059b58146cfbd2eb63916d8682f2ea94
Mike <mike63443@gmail.com>
2017-08-20 18:43:35 +0100

    Use position negotiation in Arranger
    Hopefully saving some hassle.
    
    As part of this, stop "pre-grouping" child nodes. That is, grouping as in what
    the ChildrenConsecutor was doing - splitting off consecutive children into
    their own groups. This was really useful before Node#position_after, but
    shouldn't be again until we think about parallelising work

--- app/arranger.rb
+++ app/arranger.rb
@@ -1,5 +1,5 @@
-require "children_consecutor"
 require "children_measurer"
+require "no_preceding_sibling"
 require "node_types"
 require "text_wrapper"
 
@@ -42,47 +42,17 @@ class Arranger
   def arrange_children(children)
     return [] if children.empty?
 
-    ChildrenConsecutor.new(children).as_groups
-      .inject([]) { |arranged_children, group|
-        preceding_sibling = arranged_children.last
-
-        if preceding_sibling.nil?
-          arranged_children + arrange_group(group, y: 0)
-        else
-          arranged_children + arrange_group(group, y: preceding_sibling.bottom)
-        end
-      }
-  end
-
-  def arrange_group(group, y:)
-    group.inject([]) { |arranged_children, child|
+    children.inject([]) { |arranged_children, child|
       preceding_sibling = arranged_children.last
 
-      if preceding_sibling.nil?
-        positioned_child = position_on_new_row(child, y: y)
-      else
-        positioned_child = position_on_existing_row(
-          child,
-          preceding_sibling: preceding_sibling,
-        )
-      end
-
-      arranged_children + [*call(positioned_child)]
+      arranged_children + [
+        *call(child.position_after(preceding_sibling || no_preceding_sibling))
+      ]
     }
   end
 
-  def position_on_new_row(child, y:)
-    child.clone_with(
-      x: 0,
-      y: y,
-    )
-  end
-
-  def position_on_existing_row(child, preceding_sibling:)
-    child.clone_with(
-      x: preceding_sibling.right,
-      y: preceding_sibling.y,
-    )
+  def no_preceding_sibling
+    NoPrecedingSibling.new
   end
 
   def measure_children_dimensions(children)
