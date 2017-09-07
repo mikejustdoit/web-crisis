93540397c6a39cdff2df5d3cd88dcae26963b534
Mike <mike63443@gmail.com>
2017-09-07 18:48:02 +0100

    Introduce error for unrecognised node types
    We're trying to move away from implementations tied to specific classes, so for
    the remaining places where this is happening, hopefully this can provide some
    sort of reassurance

--- app/node_types.rb
+++ app/node_types.rb
@@ -3,6 +3,8 @@ require "element"
 require "inline_element"
 require "text"
 
+class UnrecognisedNodeType < TypeError; end
+
 ELEMENTS = [BlockLevelElement, Element, InlineElement]
 
 INLINE_NODES = [InlineElement, Text]
