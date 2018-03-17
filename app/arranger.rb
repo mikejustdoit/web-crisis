ca5f7e38c05f016b353a0d2016f8c683067efb91
Mike <mike63443@gmail.com>
2018-03-17 11:52:12 +0000

    Adjust Text and its ancestors after wrapping
    Or at least attempt to. This shouldn't make a difference right now, but once
    we're wrapping text within its ancestors' maximum bounds (which are positioned
    relatively to the starting position of the Text node) this should undo the
    bizarro relative positioning we'll have applied during the process

--- app/arranger.rb
+++ app/arranger.rb
@@ -24,17 +24,45 @@ class Arranger
     arranged_children = arrange_children(positioned_node.children)
 
     inner_width, inner_height = measure_children_dimensions(arranged_children)
+    x_offset, y_offset = minimum_children_position_offset(arranged_children)
+
+    repositioned_children = arranged_children.map { |child|
+      child.clone_with(
+        x: child.x - x_offset,
+        y: child.y - y_offset,
+      )
+    }
 
     positioned_node.clone_with(
-      children: arranged_children,
+      children: repositioned_children,
+      x: positioned_node.x + x_offset,
+      y: positioned_node.y + y_offset,
       width: inner_width,
       height: inner_height,
     )
   end
 
   def visit_text(positioned_node)
-    TextWrapper.new(positioned_node, text_width_calculator: text_width_calculator)
+    wrapped_text = TextWrapper.new(
+      positioned_node,
+      text_width_calculator: text_width_calculator,
+    )
       .call(right_limit: viewport_width)
+
+    x_offset, y_offset = minimum_children_position_offset(wrapped_text.rows)
+
+    repositioned_rows = wrapped_text.rows.map { |row|
+      row.clone_with(
+        x: row.x - x_offset,
+        y: row.y - y_offset,
+      )
+    }
+
+    wrapped_text.clone_with(
+      rows: repositioned_rows,
+      x: wrapped_text.x + x_offset,
+      y: wrapped_text.y + y_offset,
+    )
   end
 
   def arrange_children(children)
@@ -55,5 +83,9 @@ class Arranger
 
   def measure_children_dimensions(children)
     ChildrenMeasurer.new.call(children)
+  end
+
+  def minimum_children_position_offset(children)
+    return children.map(&:x).min || 0, children.map(&:y).min || 0
   end
 end
