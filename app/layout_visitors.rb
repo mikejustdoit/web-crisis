07548d5db71e6e96bc686f9b4c9b71c7f0dd21dd
Mike <mike63443@gmail.com>
2018-08-02 21:21:46 +0100

    Include ImageFetcher in LAYOUT_VISITORS

--- app/layout_visitors.rb
+++ app/layout_visitors.rb
@@ -1,11 +1,13 @@
 require "absolute_position_adder_upper"
 require "arranger"
 require "block_level_reverse_inheritor"
+require "image_fetcher"
 require "layout_pipeline"
 require "root_node_dimensions_setter"
 
 LAYOUT_VISITORS = LayoutPipeline.new(
   [
+    ImageFetcher.method(:new),
     BlockLevelReverseInheritor.method(:new),
     RootNodeDimensionsSetter.method(:new),
     Arranger.method(:new),
