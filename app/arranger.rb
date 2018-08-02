f295d81b60332d4dd76207743a2cb5bc8110b2ed
Mike <mike63443@gmail.com>
2018-08-02 21:24:57 +0100

    Don't "arrange" images like other node types
    As a child of an Element, it should have its position set in relation to its
    context, but it doesn't have any children to arrange and it shouldn't be resize
    at this point in time

--- app/arranger.rb
+++ app/arranger.rb
@@ -14,6 +14,8 @@ class Arranger
       visit_element(node)
     elsif node.respond_to?(:rows)
       visit_text(node)
+    elsif node.respond_to?(:src)
+      node
     else
       raise UnrecognisedNodeType.new
     end
