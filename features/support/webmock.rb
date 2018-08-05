95ce8bffdfe3dbb885000c3a2eed3eb20b393929
Mike <mike63443@gmail.com>
2018-08-05 11:06:25 +0100

    Stub HTTP responses for assets on test webpage
    As with the origin "raw response" file, these were generated using
    
      curl --include --silent <url>

--- features/support/webmock.rb
+++ features/support/webmock.rb
@@ -5,6 +5,9 @@ WebMock.disable_net_connect!
 def raw_responses
   {
     "https://lwn.net/" => -> { File.new("features/support/lwn-raw-response.txt") },
+    "https://static.lwn.net/images/logo/barepenguin-70.png" => -> { File.new("features/support/lwn-logo-barepenguin-70-raw-response.txt") },
+    "https://static.lwn.net/images/lcorner-ss.png" => -> { File.new("features/support/lwn-lcorner-ss-raw-response.txt") },
+    "https://static.lwn.net/images/2016/03-carhackers-cover-sm.png" => -> { File.new("features/support/lwn-2016-03-carhackers-cover-sm-raw-response.text") },
   }
 end
 
