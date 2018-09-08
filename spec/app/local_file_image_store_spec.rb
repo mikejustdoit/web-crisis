5129ae0c732cf91a2957be8fa132afeb951db865
Mike <mike63443@gmail.com>
2018-09-08 13:01:45 +0100

    Document existing behaviour more thoroughly
    Before making further changes to the LocalFileImageStore, some of its
    unintentional features should be described in these specs

--- spec/app/local_file_image_store_spec.rb
+++ spec/app/local_file_image_store_spec.rb
@@ -14,4 +14,20 @@ RSpec.describe LocalFileImageStore do
       expect(image_filename).to eq("/home/seppel/art.jpg")
     end
   end
+
+  context "without the file:// scheme" do
+    it "uses the URI as the file path" do
+      image_filename = store["/home/seppel/art.jpg"]
+
+      expect(image_filename).to eq("/home/seppel/art.jpg")
+    end
+  end
+
+  context "when the URI isn't even a string" do
+    it "falls back to our placeholder image" do
+      image_filename = store[nil]
+
+      expect(image_filename).to eq(PLACEHOLDER_IMAGE)
+    end
+  end
 end
