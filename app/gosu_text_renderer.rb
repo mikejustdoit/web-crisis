5353e76505e0187d4e0571a0b85180a1a3820992
Mike <mike63443@gmail.com>
2018-09-11 07:05:53 +0100

    Rename parameters and helper method
    I'm planning to make these named parameters, but those names reflect
    GosuTextRenderer's internal dealings rather than anything its caller
    should have to know

--- app/gosu_text_renderer.rb
+++ app/gosu_text_renderer.rb
@@ -5,9 +5,9 @@ class GosuTextRenderer
     @viewport = viewport
   end
 
-  def call(text, viewport_world_x, viewport_world_y)
-    translate(viewport_world_x, viewport_world_y) do |x, y|
-      font.draw(text, x, y, 0, 1.0, 1.0, colour)
+  def call(text, x, y)
+    translate_viewport_to_gui(x, y) do |gui_x, gui_y|
+      font.draw(text, gui_x, gui_y, 0, 1.0, 1.0, colour)
     end
   end
 
@@ -15,7 +15,7 @@ class GosuTextRenderer
 
   attr_reader :viewport
 
-  def translate(x, y, &block)
+  def translate_viewport_to_gui(x, y, &block)
     block.call(viewport.x + x, viewport.y + y)
   end
 
