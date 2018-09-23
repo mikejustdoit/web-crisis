6f0d96133c0332ada0a19695e9062eb893a18fea
Mike <mike63443@gmail.com>
2018-09-23 17:29:29 +0100

    Extract Data URI as its own thing
    There's a bit too much, too case specific code in the ImageStore,
    hopefully this will help clean it up

---
+++ app/data_uri.rb
@@ -0,0 +1,36 @@
+require "base64"
+
+class DataUri
+  DATA_SCHEME_PATTERN = /^data:/
+
+  def initialize(uri)
+    @uri = uri
+  end
+
+  def write_to_file
+    if !File.exist?(name)
+      File.open(name, "wb") { |file|
+        file.print(Base64.strict_decode64(just_the_data))
+      }
+    end
+  end
+
+  def name
+    File.join(
+      ASSETS,
+      "#{Digest::SHA256.hexdigest(just_the_data)}.#{file_type}",
+    )
+  end
+
+  private
+
+  attr_reader :uri
+
+  def just_the_data
+    uri.sub(/#{DATA_SCHEME_PATTERN}image\/[a-z]+;base64,/, "")
+  end
+
+  def file_type
+    /#{DATA_SCHEME_PATTERN}image\/([a-z]+);base64,/.match(uri)[1]
+  end
+end
