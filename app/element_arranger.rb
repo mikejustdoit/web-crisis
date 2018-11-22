46fc328706762869f44c2aad87a88c4ccd9b7287
Mike <mike63443@gmail.com>
2018-11-22 09:38:12 +0000

    Use Element#clone_with_children in ElementArranger

--- app/element_arranger.rb
+++ app/element_arranger.rb
@@ -1,4 +1,3 @@
-require "children_measurer"
 require "no_preceding_sibling"
 require "node_within_parent"
 
@@ -9,29 +8,24 @@ class ElementArranger
   end
 
   def call
+    positioned_node.clone_with_children(reposition_children)
+  end
+
+  def reposition_children
     arranged_children = arrange_children(
       positioned_node.children.map { |child|
         NodeWithinParent.new(child, positioned_node)
       }
     )
 
-    inner_width, inner_height = measure_children_dimensions(arranged_children)
     x_offset, y_offset = minimum_children_position_offset(arranged_children)
 
-    repositioned_children = arranged_children.map { |child|
+    arranged_children.map { |child|
       child.clone_with(
         x: child.x - x_offset,
         y: child.y - y_offset,
       )
     }
-
-    positioned_node.clone_with(
-      children: repositioned_children,
-      x: positioned_node.x + x_offset,
-      y: positioned_node.y + y_offset,
-      width: inner_width,
-      height: inner_height,
-    )
   end
 
   private
@@ -54,10 +48,6 @@ class ElementArranger
 
   def no_preceding_sibling
     NoPrecedingSibling.new
-  end
-
-  def measure_children_dimensions(children)
-    ChildrenMeasurer.new.call(children)
   end
 
   def minimum_children_position_offset(children)
