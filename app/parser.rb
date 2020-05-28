b93aab7c242261b4bbd130b53d3f98b9efb31260
Mike <mike63443@gmail.com>
2020-05-28 08:24:21 +0100

    List supported MIME types on Parser class
    There feels like a difference between having one of these live on Image
    (a node/element entity) and having this live here. Not happy but also
    want to move forward :D

--- app/parser.rb
+++ app/parser.rb
@@ -3,11 +3,17 @@ require "nokogiri"
 require "once_per_unique_call"
 
 class Parser
+  SUPPORTED_MIME_TYPES = %w{text/html}
+
   def call(html)
     NodeFactory.new(
       Nokogiri::HTML(html).at_css("html"),
       logger: throttled_logger,
     ).call
+  end
+
+  def supported_mime_types
+    SUPPORTED_MIME_TYPES
   end
 
   private
