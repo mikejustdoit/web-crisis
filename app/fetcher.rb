11648b3f5cd963248b116cc7a5944fd36505a5ac
Mike <mike63443@gmail.com>
2020-05-30 08:10:33 +0100

    Remove misdirection

--- app/fetcher.rb
+++ app/fetcher.rb
@@ -2,16 +2,6 @@ require "rest-client"
 
 class Fetcher
   def call(uri)
-    get_body(uri)
-  end
-
-  private
-
-  def get_body(uri)
-    rest_client.get(uri).body
-  end
-
-  def rest_client
-    RestClient
+    RestClient.get(uri).body
   end
 end
