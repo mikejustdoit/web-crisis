e896ea4a059428c20d6ab29d23a1c025b4a4bb02
Mike <mike63443@gmail.com>
2021-05-01 14:46:24 +0100

    Tidy up spec a little bit
    * introduce instance_doubles, object_doubles and partial doubles
    * rephrase some things and remove some unhelpful `describe`s
    * put the big messy duplicated execution step in a method
    * keep the assertions as loose and possibly pointless as they were

--- spec/app/engine_spec.rb
+++ spec/app/engine_spec.rb
@@ -1,5 +1,9 @@
+require "drawing_visitors"
 require "element"
 require "engine"
+require "fetcher"
+require "layout_visitors"
+require "parser"
 
 RSpec.describe Engine do
   subject(:engine) {
@@ -12,69 +16,48 @@ RSpec.describe Engine do
     )
   }
 
-  let(:drawing_visitors) { double(:drawing_visitors, :visit => a_tree) }
-  let(:fetcher) { double(:fetcher, :call => nil) }
-  let(:image_store_factory) {
-    double(
-      :image_store_factory,
-      :call => double(:image_store, :call => nil),
-    )
-  }
-  let(:layout_visitors) { double(:layout_visitors, :visit => a_tree) }
-  let(:parser) {
-    double(:parser, :call => nil, :supported_mime_types => %w{text/html})
-  }
+  let(:drawing_visitors) { object_double(DRAWING_VISITORS, :visit => a_tree) }
+  let(:fetcher) { instance_double("Fetcher", :call => "<html></html>") }
+  let(:image_store_factory) { double(:image_store_factory, :call => nil) }
+  let(:layout_visitors) { object_double(LAYOUT_VISITORS, :visit => nil) }
+  let(:parser) { Parser.new.tap { |p| allow(p).to receive(:call) } }
 
   let(:a_tree) { Element.new }
 
-  describe "relationship with its collaborators" do
-    before do
-      engine.uri = "http://www.drchip.org/astronaut/vim/index.html"
+  def render
+    engine.uri = "http://www.drchip.org/astronaut/vim/index.html"
 
-      engine.render(
-        viewport_width: 640,
-        viewport_height: 480,
-        text_calculator: double(:text_calculator),
-        image_calculator: double(:image_calculator),
-        box_renderer: double(:box_renderer),
-        image_renderer: double(:image_renderer),
-        text_renderer: double(:text_renderer),
-      )
-    end
-
-    it "calls its callable collaborators" do
-      expect(fetcher).to have_received(:call)
-      expect(parser).to have_received(:call)
-    end
-
-    it "builds its image store with the current 'origin' URI" do
-      expect(image_store_factory).to have_received(:call)
-        .with(origin: "http://www.drchip.org/astronaut/vim/index.html")
-    end
-
-    it "starts its visitors visiting" do
-      expect(layout_visitors).to have_received(:visit)
-      expect(drawing_visitors).to have_received(:visit)
-    end
+    engine.render(
+      viewport_width: 640,
+      viewport_height: 480,
+      text_calculator: double(:text_calculator),
+      image_calculator: double(:image_calculator),
+      box_renderer: double(:box_renderer),
+      image_renderer: double(:image_renderer),
+      text_renderer: double(:text_renderer),
+    )
   end
 
-  describe "#fetch returning a layed-out tree" do
-    let(:returned_tree) {
-      engine.uri = "http://www.drchip.org/astronaut/vim/index.html"
+  it "fetches its current URI and parses it" do
+    render
 
-      engine.render(
-        viewport_width: 640,
-        viewport_height: 480,
-        text_calculator: double(:text_calculator),
-        image_calculator: double(:image_calculator),
-        box_renderer: double(:box_renderer),
-        image_renderer: double(:image_renderer),
-        text_renderer: double(:text_renderer),
-      )
-    }
+    expect(fetcher).to have_received(:call)
+    expect(parser).to have_received(:call)
+  end
 
-    it "returns a tree alright" do
-      expect(returned_tree).to respond_to(:children)
-    end
+  it "builds its image store with the current 'origin' URI" do
+    render
+
+    expect(image_store_factory).to have_received(:call)
+      .with(origin: "http://www.drchip.org/astronaut/vim/index.html")
+  end
+
+  it "lays out and draws the parsed render tree and returns it" do
+    returned_tree = render
+
+    expect(layout_visitors).to have_received(:visit)
+    expect(drawing_visitors).to have_received(:visit)
+
+    expect(returned_tree).to eq(a_tree)
   end
 end
