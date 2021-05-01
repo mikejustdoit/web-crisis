0deb19b01d4e5785a7bb9ada5306065f0d2adeda
Mike <mike63443@gmail.com>
2021-05-01 12:53:54 +0100

    Set default text dimensions and apply overrides
    In the feature tests there's a single scenario where we force the screen
    size of text to be so big that it forces text to be wrapped over
    mutliple rows.
    
    I'm not convinced that it's a good way to test the feature, but until
    I'm certain it's not I'm constantly looking for better ways to support
    it :D

--- spec/support/window_double.rb
+++ spec/support/window_double.rb
@@ -2,10 +2,12 @@ require "box"
 require "gosu_adapter_stubs"
 
 class WindowDouble
+  include ::RSpec::Mocks::ExampleMethods
+
   def initialize(engine:)
     @engine = engine
-    @fixed_width_for_word_or_space = 50
     @height = 480
+    @text_calculator = gosu_text_calculator_stub(returns: [30, 18])
     @width = 640
   end
 
@@ -17,9 +19,7 @@ class WindowDouble
     engine.render(
       viewport_width: viewport.width,
       viewport_height: viewport.height,
-      text_calculator: gosu_text_calculator_stub(
-        returns: [fixed_width_for_word_or_space, 18]
-      ),
+      text_calculator: text_calculator,
       image_calculator: gosu_image_calculator_stub(returns: [100, 100]),
       box_renderer: gosu_box_renderer_stub,
       image_renderer: gosu_image_renderer_stub,
@@ -28,7 +28,11 @@ class WindowDouble
   end
 
   def allow_words_per_row(n)
-    @fixed_width_for_word_or_space = width / n
+    fixed_width_for_word_or_space = width / n
+
+    allow(text_calculator).to receive(:call).and_return(
+      [fixed_width_for_word_or_space, 18]
+    )
   end
 
   def in_view?(node)
@@ -37,7 +41,11 @@ class WindowDouble
 
   private
 
-  attr_reader :engine, :fixed_width_for_word_or_space, :height, :width
+  attr_reader :engine,
+    :fixed_width_for_word_or_space,
+    :height,
+    :text_calculator,
+    :width
 
   def viewport
     Box.new(x: 0, y: 0, width: width, height: height)
