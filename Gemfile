5e61b0bf032242a11df8f4468748f7bf0f1fdc0b
Mike <mike63443@gmail.com>
2021-05-06 18:33:53 +0100

    Install VCR gem
    With a few reasonable sounding configuration options.
    
    Plus header whitelisting for recorded requests and responses

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source "https://rubygems.org"
 
 gem "cucumber"
 gem "rspec"
+gem "vcr"
 gem "webmock"
 
 gem "gosu"
--- features/support/network.rb
+++ features/support/network.rb
@@ -1,3 +1,4 @@
+require "vcr"
 require "webmock/cucumber"
 
 WebMock.disable_net_connect!
@@ -22,5 +23,23 @@ end
 Before do
   raw_responses.each do |uri, raw_response|
     stub_request(:get, uri).to_return { raw_response.call }
+  end
+end
+
+VCR.configure do |config|
+  config.cassette_library_dir = "fixtures/vcr_cassettes"
+  config.hook_into :webmock
+  config.default_cassette_options = { update_content_length_header: true }
+
+  allow_request_headers = []
+  allow_response_headers = %q{ Content-Length Content-Type }
+
+  config.before_record do |interaction, _cassette|
+    interaction.request.headers.keep_if { |key, _value|
+      allow_request_headers.include?(key)
+    }
+    interaction.response.headers.keep_if { |key, _value|
+      allow_response_headers.include?(key)
+    }
   end
 end
