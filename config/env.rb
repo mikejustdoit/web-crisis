a9c171c54ffc260a5f209dd6e948b7a1eaf7d23a
Mike <mike63443@gmail.com>
2021-05-07 13:39:10 +0100

    Iron out ".."s from file path constant

--- config/env.rb
+++ config/env.rb
@@ -5,4 +5,6 @@ $LOAD_PATH.unshift(File.expand_path("app"))
 
 ASSETS = File.absolute_path(File.join(File.dirname(__FILE__), "..", "assets"))
 
-PLACEHOLDER_IMAGE = File.join(File.dirname(__FILE__), "..", "app", "broken.png")
+PLACEHOLDER_IMAGE = File.absolute_path(
+  File.join(File.dirname(__FILE__), "..", "app", "broken.png")
+)
