5b70a1ece21985356eca12558f0898beb83b1637
Mike <mike63443@gmail.com>
2021-05-08 14:33:10 +0100

    Run ScrollPositioner before drawing to screen

--- app/drawing_visitors.rb
+++ app/drawing_visitors.rb
@@ -1,10 +1,12 @@
 require "image_drawing_visitor"
 require "root_node_drawing_visitor"
+require "scroll_positioner"
 require "text_drawing_visitor"
 require "visitor_pipeline"
 
 DRAWING_VISITORS = VisitorPipeline.new(
   [
+    ScrollPositioner.method(:new),
     RootNodeDrawingVisitor.method(:new),
     TextDrawingVisitor.method(:new),
     ImageDrawingVisitor.method(:new),
